f94137ca7dc33ab726dc985faaf82d8b3f286484
Jakob <jped22@student.aau.dk>
2026-02-05 11:31:37 +0100

    Ups, Mads slettede mine ændringer
    Men de burde være tilbage igen

---
+++ Assets/SceneManage.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
