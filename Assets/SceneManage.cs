70fc94017de002bc9089a07b136eb35e3e96eb18
Jakob <jped22@student.aau.dk>
2026-02-05 12:36:46 +0100

    Scene for loading scenes

--- Assets/SceneManage.cs
+++ Assets/SceneManage.cs
@@ -1,15 +1,35 @@
+using NUnit.Framework;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class SceneManage : MonoBehaviour
 {
-    void Start()
+    private static SceneManage _instance;
+
+    void Awake()
     {
-        
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 
-    void Update()
+
+    void OnPress1(InputValue value)
     {
-        
+        SceneManager.LoadScene(1);
+    }
+
+    void OnPress2(InputValue value)
+    {
+        SceneManager.LoadScene(2);
     }
 }
