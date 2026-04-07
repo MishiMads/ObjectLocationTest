7f1521e58fce67d9d41a5a4ee2ae3854e2e7a30b
Jakob <jped22@student.aau.dk>
2026-04-07 23:43:57 +0200

    Items sender en marker gennem LSL nu :D

--- Assets/Interactables Things/InteractionTest.cs
+++ Assets/Interactables Things/InteractionTest.cs
@@ -1,12 +1,28 @@
+using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using static UnityEditor.Rendering.CameraUI;
 
 public class InteractionTest : MonoBehaviour
 {
     public InteractablesHandler interactableHandler;
+    public SceneManage sceneManage;
+
+    public string itemName;
+
+    private void Start()
+    {
+        sceneManage = GameObject.FindGameObjectWithTag("Manager").GetComponent<SceneManage>();
+    }
 
     public void DestroySelf()
     {
         interactableHandler.foundCounter++;
         Destroy(gameObject);
+    }
+
+    //LSL Marker send function
+    public void SendMarker()
+    {
+        sceneManage.LSLSend(itemName);
     }
 }
--- Assets/SceneManage.cs
+++ Assets/SceneManage.cs
@@ -3,12 +3,19 @@ using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using LSL;
 
 public class SceneManage : MonoBehaviour
 {
     private static SceneManage _instance;
 
     public bool itemsVisible = false;
+
+    //LSL Stream Stuff
+    string StreamName = "InteractionEvent";
+    string StreamType = "Markers";
+    private StreamOutlet outlet;
+    private string[] sample = { "" };
 
     void Awake()
     {
@@ -22,6 +29,24 @@ public class SceneManage : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        var hash = new Hash128();
+        hash.Append(StreamName);
+        hash.Append(StreamType);
+        hash.Append(gameObject.GetInstanceID());
+        StreamInfo streamInfo = new StreamInfo(StreamName, StreamType, 1, LSL.LSL.IRREGULAR_RATE,
+            channel_format_t.cf_string, hash.ToString());
+        outlet = new StreamOutlet(streamInfo);
+    }
+
+    public void LSLSend(string itemName)
+    {
+        sample[0] = "#i;RhythmicUrban;" + itemName + ";Selected";
+        Debug.Log(sample[0]);
+        outlet.push_sample(sample);
     }
 
 
